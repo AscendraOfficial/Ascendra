8ab64c617e613c24d4d5f4eaa43a969042b81140
ess <alan.kai.chong@gmail.com>
2026-08-01 11:49:45 -0600

    Add journal backend

--- backend/app.py
+++ backend/app.py
@@ -1,19 +1,34 @@
 # Importing
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
+import json 
 
 # Starting up
 app = Flask(__name__)
 CORS(app)
 
+# Constants
+JOURNAL_FILE = "storage/journal.json"
+
+# ---------------
+
+def loadJournal():
+    with open(JOURNAL_FILE, "r") as file:
+        return json.load(file)
+
+def saveJournal(journal):
+    with open(JOURNAL_FILE, "w") as file:
+        json.dump(journal, file)
+
 # ---------------
 
 @app.get("/")
 def health():
-    return {
-        "status": "online",
-        "app": "Ascendra"
-    }
+    return loadJournal()
+
+@app.post("/journal")
+def postJournal():
+    journal = loadJournal()
 
 
 
