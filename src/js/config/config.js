d6be6119331c883d129ecf5e400edbc823e9394a
ess <alan.kai.chong@gmail.com>
2026-08-16 14:56:18 -0600

    Bump version to 1.1.1-beta

--- src/js/config/config.js
+++ src/js/config/config.js
@@ -1,17 +1,13 @@
 const APP_CONFIG = {
   name: "Ascendra",
-  version: "1.1.0-beta",
+  version: "1.1.1-beta",
   update: "Horizon",
 };
 
 let API_URL;
 
 // If running from the file system (file://) or served locally, use local backend for development
-if (
-  window.location.protocol === "file:" ||
-  window.location.hostname === "127.0.0.1" ||
-  window.location.hostname === "localhost"
-) {
+if (window.location.protocol === "file:" || window.location.hostname === "127.0.0.1" || window.location.hostname === "localhost") {
   API_URL = "http://127.0.0.1:5000";
 } else {
   API_URL = "https://ascendra-oc22.onrender.com";
