cb313fa14db8141cfa9d20d7518626a38761974f
jaydenwcaau-collab <jaydenwcaau@gmail.com>
2026-09-24 20:02:24 +1000

    Update merchandise(KidsMode).js

--- Ascendra/merchandise(KidsMode).js
+++ Ascendra/merchandise(KidsMode).js
@@ -2,13 +2,11 @@ function showSection(sectionId) {
 
     const sections = document.querySelectorAll(".page-section");
 
-  sections.forEach(section => {
-      section.style.display = "none";
+    sections.forEach(section => {
+        section.style.display = "none";
     });
 
     document.getElementById(sectionId).style.display = "block";
 }
 
-
-// Show Merchandise when the page first opens
 showSection("merchandiseSection");
