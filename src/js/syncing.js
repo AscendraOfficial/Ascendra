d28bae2228e1e0b4a1baa9029f42a2cacc2a88ad
ess <alan.kai.chong@gmail.com>
2026-09-26 09:23:35 -0600

    feat: add html for syncing

--- src/js/syncing.js
+++ src/js/syncing.js
@@ -1,6 +1,6 @@
 "use strict";
 
-sync_statuses = {
-    manual_sync_statuses: ["Synced", "Yet to be synced"],
-    auto_sync_statuses: ["Synced", "Syncing"]
-}
+const sync_statuses = {
+  manual_sync_statuses: ["Synced", "Not synced"],
+  auto_sync_statuses: ["Synced", "Syncing"],
+};
